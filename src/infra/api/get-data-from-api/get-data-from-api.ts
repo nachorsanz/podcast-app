b3d01d6ea324f4847a82fec96d260b24c7b4f49e
nachorsanz <nachorsanz@gmail.com>
2023-04-20 18:17:43 +0200

    chore: last deploy

--- src/infra/api/get-data-from-api/get-data-from-api.ts
+++ src/infra/api/get-data-from-api/get-data-from-api.ts
@@ -1,22 +1,16 @@
-
-const topPodcastsUrl = process.env.VITE_APP_PODCAST_MAIN_URL as string
-const detailPodcastUrl = process.env.VITE_APP_PODCAST_DETAIL_URL as string
+const topPodcastsUrl = process.env.VITE_APP_PODCAST_MAIN_URL as string;
+const detailPodcastUrl = process.env.VITE_APP_PODCAST_DETAIL_URL as string;
 
 export const getTopPodcasts = async () => {
-  const response = await fetch(
-    topPodcastsUrl,
-  );
+  const response = await fetch(topPodcastsUrl);
 
   const data = await response?.json();
   return data;
 };
 
 export const getPodcastDetail = async (podcastId: string) => {
-  const response = await fetch(
-    `${detailPodcastUrl}${podcastId}`,
-  );
-  
-const data = await response?.json();
-return data;
- 
+  const response = await fetch(`${detailPodcastUrl}${podcastId}`);
+
+  const data = await response?.json();
+  return data;
 };
